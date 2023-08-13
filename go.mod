04d8be1dd007ae587b17cb8698ff41c7e85c6bb5
Fakhrulzaki94 <Fakhrulzaki94@yahoo.com>
2023-08-13 22:00:49 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fakhrulzaki94/go-logger
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,17 @@
+package logger
+
+import (
+	"log"
+)
+
+func LogInfo(message string) {
+	log.Printf("INFO - %v", message)
+}
+
+func LogWarning(message string) {
+	log.Printf("WARNING - %v", message)
+}
+
+func LogError(message string) {
+	log.Printf("ERROR - %v", message)
+}
